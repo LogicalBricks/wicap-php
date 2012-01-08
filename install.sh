a7176d364365b9dc7d3c3b4f19b615eb1ba62104
Antonio Hernández Blas <hba.nihilismus@gmail.com>
2012-01-08 03:15:18 -0600

    Improvements in the way wicap-php is installed. The install.sh script should be executed with root permissions.

--- install.sh
+++ install.sh
@@ -1,72 +1,72 @@
 #!/bin/sh
 
 # careful, not really tested
+# ... needs more testing
 
-# configuration 
+# Configuration
 basedir=/var/www
 target=usr/local/wicap-php
-dest=$basedir/$target
+dest=${basedir}/${target}
 rrdtool=/usr/local/bin/rrdtool
-# end configuration
 
-echo "-> I am going to compile the daemon..."
-cd source
-make
-cd ..
-
-echo "-> I am going to install some files at $dest..."
-echo -n "-> do you want to continue (y or n) ? "
-read yn
-if [ $yn != "y" ];then
-        echo "-> okay, goodbye."
-        exit;
+uid=$(/usr/bin/id -ur)
+if [ ${uid} != 0 ]; then
+	echo "-> Error, you do not have root permissions"
+	exit 1
 fi
 
-if [ ! -d $dest ];then
-	echo "-> Making Directories..."
-	mkdir -p $dest
-	mkdir $dest/var
-	mkdir $dest/bin
-	mkdir $dest/htdocs
+echo "-> Compiling 'allow' daemon..."
+( cd source
+make clean all
+) || exit 1
 
-	echo "-> Copying Files..."
-	cp source/allow $dest/bin/
-	cp source/allow.rc $dest/bin/
-	cp source/rotate_something.sh $dest/bin/
-	cp source/rotate.cron $dest/bin/
-	cp source/htdocs/*.php $dest/htdocs/
-	cp source/htdocs/*.jpg $dest/htdocs/
-		
-	echo "-> Setting Permissions..."
-	chown www $dest/var
-	chmod 700 $dest/bin/allow
-	chmod 700 $dest/bin/allow.rc
-	chmod 700 $dest/bin/rotate.cron
-	chmod 700 $dest/bin/rotate_something.sh
+echo "-> Creating '$dest' structure"
+mkdir -p ${dest}/{var,bin,htdocs} || exit 1
 
-	echo "-> Do you want me to create a Round-Robin-Database (RRD)"
-	echo -n "-> for stats? (rrdtool must be installed) (y or n)? "
-	read yn
-	if [ $yn != "y" ];then
-		echo "Okay, I didn't do it"
-	else
-		echo "Making RRD..."
-		./mkrrd.sh $rrdtool $dest/var/stats.rrd
+echo "-> Copying files to '$dest'"
+cp source/allow ${dest}/bin
+cp source/allow.rc ${dest}/bin
+cp source/rotate_something.sh ${dest}/bin
+cp source/rotate.cron ${dest}/bin
+cp source/htdocs/*.php ${dest}/htdocs
+cp source/htdocs/*.jpg ${dest}/htdocs
+
+echo "-> Setting permissions to '$dest'"
+chown -R www ${dest}/var
+chmod 700 ${dest}/bin/*
+
+#echo "-> Do you want me to create a Round-Robin-Database (RRD)"
+#echo -n "-> for stats? (rrdtool must be installed) (y or n)? "
+#read yn
+#if [ $yn != "y" ];then
+#	echo "Okay, I didn't do it"
+#else
+#	echo "Making RRD..."
+#	./mkrrd.sh $rrdtool $dest/var/stats.rrd
+#fi
+
+( cd $dest/htdocs
+  echo "-> Linking..."
+  echo "   $dest/htdocs/wicap.php -> $dest/htdocs/index.php"
+  ln -sf wicap.php index.php
+  echo "-> Linking..."
+  echo "   $dest -> /$target"
+  ln -sf $dest /$target
+) || exit 1
+
+echo "-> Installing..."
+echo "   source/conf/httpd-minimal.conf as ${basedir}/conf/httpd.conf"
+if [ -f ${basedir}/conf/httpd.conf ]; then
+	osha256=$(/bin/sha256 -q ${basedir}/conf/httpd.conf)
+	nsha256=$(/bin/sha256 -q source/conf/httpd-minimal.conf)
+	if [ "${osha256}" != "${nsha256}" ]; then
+		echo "-> Error, ${basedir}/conf/httpd.conf differs from"
+		echo "   source/conf/httpd-minimal.conf"
+		echo "   Make a backup of ${basedir}/conf/httpd.conf, delete it"
+		echo "   and execute once again this shell script"
+		exit 1
 	fi
-
-	echo "-> Creating Symbolic-Links..."
-	cd $dest/htdocs
-	ln -s wicap.php index.php
-	ln -s $dest /$target
-else
-	echo
-	echo "-> Fatal: You have something at $dest..."
-	echo "-> perhaps this means you are upgrading...in any case"
-	echo "-> you should move/delete this directory first, and then"
-	echo "-> rerun this script"
-
-	exit 
 fi
+cat source/conf/httpd-minimal.conf > ${basedir}/conf/httpd.conf
 
-echo "-> Okay, All done!"
-echo "-> You can use $dest/bin/allow.rc to start the daemon"
+echo "-> Done"

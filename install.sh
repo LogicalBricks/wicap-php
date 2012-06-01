a1cce6e8f33eca3141aae025403120f3a74dbd47
Fitorec <chanerec@gmail.com>
2012-05-31 23:03:55 -0500

    Organizing the logic of the install script.
     Author:      @Fitorec <chanerec@gmail.com>
     Date:        jue may 31 23:03:55 CDT 2012
     Description: I fit into the logic functions of the install.sh script, I defined the variable XD so that the installation script can be run from outside the directory, for example:
    
    	#Unzip the package
    	tar -czf LogicalBricks-wicap-php-aae2b86.tar.gz
    	#run install.sh
    	sh LogicalBricks-wicap-php-aae2b86/install.sh
    
     -------------------------------------------------------------------------
     install.sh |  126 ++++++++++++++++++++++++++++++++++++++++++++----------------
     1 files changed, 93 insertions(+), 33 deletions(-)

--- install.sh
+++ install.sh
@@ -1,39 +1,102 @@
 #!/bin/sh
-
-# careful, not really tested
-# ... needs more testing
-
+#
+# careful, not really tested          _
+# ... needs more testing             (_)
+#              |    .
+#          .   |L  /|   .          _
+#      _ . |\ _| \--+._/| .       (_)
+#     / ||\| Y J  )   / |/| ./
+#    J  |)'( |        ` F`.'/        _
+#  -<|  F         __     .-<        (_)
+#    | /       .-'. `.  /-. L___      _____                 ____   _____ _____
+#    J \      <    \  | | O\|.-' _   / ___ \               |  _ \ / ____|  __ \
+#  _J \  .-    \/ O | | \  |F   (_) / /  / /___  ___  ____ | |_) | (___ | |  | |
+# '-F  -<_.     \   .-'  `-' L__   / /  / / __ \/ _ \/ __ \|  _ < \___ \| |  | |
+#__J  _   _.     >-'  )._.   |-'  / /__/ / /_/ /  __/ / / /| |_) |____) | |__| |
+#`-|.'   /_.           \_|   F    \_____/ .___/\___/_/ /_/ |____/|_____/|_____/
+#  /.-   .                _.<    ======/ /======================================
+# /'    /.'             .'  `\        /_/ Wicap Install
+#  /L  /'   |/      _.-'-\
+# /'J       ___.---'\|
+#   |\  .--' V  | `. `
+#   |/`. `-.     `._)
+#      / .-.\
+#      \ (  `\
 # Configuration
 basedir=/var/www
 target=usr/local/wicap-php
 dest=${basedir}/${target}
 rrdtool=/usr/local/bin/rrdtool
+source=$(dirname `readlink -f "$0"`)/source
+# check permissions
+#
+check_permissions (){
+	uid=$(/usr/bin/id -ur)
+	if [ ${uid} != 0 ]; then
+		echo "\n-> Error, you do not have root permissions"
+		exit 1
+	fi
+}
+check_permissions
 
-uid=$(/usr/bin/id -ur)
-if [ ${uid} != 0 ]; then
-	echo "-> Error, you do not have root permissions"
-	exit 1
-fi
+# Compile deamon
+#
+make_daemon (){
+	echo "-> Compiling 'allow' daemon..."
+	cd ${source}
+	make clean all
+	if [ ${?} != 0 ]; then
+		echo "\n-> Error while compiling the daemon"
+		echo "   You should check you have all the dependencies."
+		exit 1
+	fi
+}
+make_daemon
 
-echo "-> Compiling 'allow' daemon..."
-( cd source
-make clean all
-) || exit 1
+# Create the following structure:
+# /var/www/usr/local/wicap-php/
+#                             ├── bin
+#                             ├── htdocs
+#                             └── var
+create_structure (){
+	echo "-> Creating '${dest}' structure"
+	(mkdir -p ${dest}/var && mkdir -p ${dest}/bin && mkdir -p ${dest}/htdocs)
+	if [ ${?} != 0 ]; then
+		echo "\n-> Error while creating the structure"
+		exit 1
+	fi
+}
+create_structure
 
-echo "-> Creating '$dest' structure"
-mkdir -p ${dest}/{var,bin,htdocs} || exit 1
+# copy files
+#
+copy_files (){
+	echo "-> Copying files to '$dest'"
+	( cp ${source}/allow ${dest}/bin && \
+	cp ${source}/allow.rc ${dest}/bin && \
+	cp ${source}/rotate_something.sh ${dest}/bin && \
+	cp ${source}/rotate.cron ${dest}/bin && \
+	cp ${source}/htdocs/*.php ${dest}/htdocs && \
+	cp ${source}/htdocs/*.jpg ${dest}/htdocs )
+	if [ ${?} != 0 ]; then
+		echo "\n-> Error while Copying the files"
+		exit 1
+	fi
+}
+copy_files
 
-echo "-> Copying files to '$dest'"
-cp source/allow ${dest}/bin
-cp source/allow.rc ${dest}/bin
-cp source/rotate_something.sh ${dest}/bin
-cp source/rotate.cron ${dest}/bin
-cp source/htdocs/*.php ${dest}/htdocs
-cp source/htdocs/*.jpg ${dest}/htdocs
-
-echo "-> Setting permissions to '$dest'"
-chown -R www ${dest}/var
-chmod 700 ${dest}/bin/*
+#set permissions
+#
+set_permissions (){
+	echo "-> Setting permissions to '$dest'"
+	(chown -R www ${dest}/var && \
+	chmod 700 ${dest}/bin/* )
+	if [ ${?} != 0 ]; then
+		echo "\n-> Error while Setting permissions to '$dest'"
+		exit 1
+	fi
+}
+set_permissions
 
 #echo "-> Do you want me to create a Round-Robin-Database (RRD)"
 #echo -n "-> for stats? (rrdtool must be installed) (y or n)? "
@@ -44,7 +107,6 @@ chmod 700 ${dest}/bin/*
 #	echo "Making RRD..."
 #	./mkrrd.sh $rrdtool $dest/var/stats.rrd
 #fi
-
 ( cd $dest/htdocs
   echo "-> Linking..."
   echo "   $dest/htdocs/wicap.php -> $dest/htdocs/index.php"
@@ -53,20 +115,18 @@ chmod 700 ${dest}/bin/*
   echo "   $dest -> /$target"
   ln -sf $dest /$target
 ) || exit 1
-
 echo "-> Installing..."
-echo "   source/conf/httpd-minimal.conf as ${basedir}/conf/httpd.conf"
+echo "   ${source}/conf/httpd-minimal.conf as ${basedir}/conf/httpd.conf"
 if [ -f ${basedir}/conf/httpd.conf ]; then
 	osha256=$(/bin/sha256 -q ${basedir}/conf/httpd.conf)
-	nsha256=$(/bin/sha256 -q source/conf/httpd-minimal.conf)
+	nsha256=$(/bin/sha256 -q ${source}/conf/httpd-minimal.conf)
 	if [ "${osha256}" != "${nsha256}" ]; then
 		echo "-> Error, ${basedir}/conf/httpd.conf differs from"
-		echo "   source/conf/httpd-minimal.conf"
+		echo "   ${source}/conf/httpd-minimal.conf"
 		echo "   Make a backup of ${basedir}/conf/httpd.conf, delete it"
 		echo "   and execute once again this shell script"
 		exit 1
 	fi
 fi
-cat source/conf/httpd-minimal.conf > ${basedir}/conf/httpd.conf
-
+cat ${source}/conf/httpd-minimal.conf > ${basedir}/conf/httpd.conf
 echo "-> Done"

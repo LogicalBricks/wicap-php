031f84f4dbffad5412c6c87c5bd0632d0a0e420c
Fitorec <chanerec@gmail.com>
2012-05-31 23:42:04 -0500

    Edit the mkrrd.sh
     Author:      @Fitorec <chanerec@gmail.com>
     Date:        jue may 31 23:42:04 CDT 2012
     Description: Using which program to get the path of the rrdtool.
     -------------------------------------------------------------------------
     mkrrd.sh |   46 ++++++++++++++++++++++++++++------------------
     1 files changed, 28 insertions(+), 18 deletions(-)

--- mkrrd.sh
+++ mkrrd.sh
@@ -9,26 +9,36 @@
 # excellent tutorial on rrdtool:
 # http://people.ee.ethz.ch/~oetiker/webtools/rrdtool/tut/rrdtutorial.en.html
 
-rrdtool=/usr/local/bin/rrdtool
+rrdtool=`which rrdtool`
 rrdfile=/var/www/usr/local/wicap-php/var/stats.rrd
 
-if [ $1 ];then
-	echo "-> Using $1 for rrdtool path"
-	rrdtool=$1
+if [ ${1} ];then
+	echo "-> Using ${1} for rrdtool path"
+	rrdtool=${1}
 fi
-if [ $2 ];then
-	echo "-> Using $2 for resulting rrd"
-	rrdfile=$2
+if [ ${2} ];then
+	echo "-> Using ${2} for resulting rrd"
+	rrdfile=${2}
 fi
 
-$rrdtool create $rrdfile \
-	DS:auths:ABSOLUTE:600:U:U \
-	DS:evicts:ABSOLUTE:600:U:U \
-	RRA:AVERAGE:0.5:1:600 \
-	RRA:AVERAGE:0.5:6:700 \
-	RRA:AVERAGE:0.5:24:775 \
-	RRA:AVERAGE:0.5:288:797 \
-	RRA:MAX:0.5:1:600 \
-	RRA:MAX:0.5:6:700 \
-	RRA:MAX:0.5:24:775 \
-	RRA:MAX:0.5:288:797
+#
+#
+mkrrd (){
+	${1} create ${2} \
+		DS:auths:ABSOLUTE:600:U:U \
+		DS:evicts:ABSOLUTE:600:U:U \
+		RRA:AVERAGE:0.5:1:600 \
+		RRA:AVERAGE:0.5:6:700 \
+		RRA:AVERAGE:0.5:24:775 \
+		RRA:AVERAGE:0.5:288:797 \
+		RRA:MAX:0.5:1:600 \
+		RRA:MAX:0.5:6:700 \
+		RRA:MAX:0.5:24:775 \
+		RRA:MAX:0.5:288:797
+	if [ ${?} != 0 ]; then
+		echo "\n-> Error "
+		exit 1
+	fi
+}
+
+mkrrd ${rrdtool} ${rrdfile}
